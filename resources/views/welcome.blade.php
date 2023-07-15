df95f0d8a20994043f933743bed2cf776a509fab
ekpono <ekponoambrose@gmail.com>
2023-07-15 14:49:49 +0100

    homepage title changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>New Project for 10x developer</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
